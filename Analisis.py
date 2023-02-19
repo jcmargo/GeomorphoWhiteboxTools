e992ba44afb202ad780cca7504ba761b5b98cde0
jcmar <juancamargodatos@gmail.com>
2023-02-19 12:12:11 -0500

    Analisis.py

---
+++ Analisis.py
@@ -0,0 +1,13 @@
+#librerias a usar
+import os
+import pkg_resources
+import matplotlib.pyplot as plt
+import numpy as np
+import imageio
+##%matplotlib inline  
+
+import whitebox
+wbt = whitebox.WhiteboxTools()
+
+print(wbt.version())
+print(wbt.help())
